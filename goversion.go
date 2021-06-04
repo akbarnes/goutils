7118127066cb4af8c263726ce6031661ab4801fb
Art Barnes <32542598+akbarnes@users.noreply.github.com>
2021-06-04 07:59:33 -0600

    add commit message & snapshot

--- goversion.go
+++ goversion.go
@@ -7,8 +7,9 @@ import (
 	"io"
 	"io/ioutil"
 	"os"
-	// "time"
+	"time"
 	"path/filepath"
+	"encoding/json"
 )
 
 func check(e error) {
@@ -16,13 +17,6 @@ func check(e error) {
 		panic(e)
 	}
 }
-
-// // Optional timestamp
-// t := time.Now()
-
-// if TimeStampMode {
-// 	fmt.Print(t.Format("2006-01-02T15-04-05-"))
-// }
 
 func HashFile(FileName string, NumChars int) (string, error) {
 	var data []byte
@@ -65,28 +59,65 @@ func CopyFile(src, dst string) error {
 	return out.Close()
 }
 
+func (snap Snapshot) Write(snapshotPath string) {
+	f, err := os.Create(snapshotPath)
+
+	if err != nil {
+		panic(fmt.Sprintf("Error: Could not create snapshot file %s", snapshotPath))
+	}
+	myEncoder := json.NewEncoder(f)
+	myEncoder.SetIndent("", "  ")
+	myEncoder.Encode(snap)
+	f.Close()
+}
+
 var TimeStampMode bool
 var NumChars int
+var Message string
 
 func init() {
 	flag.BoolVar(&TimeStampMode, "t", false, "prefix with time stamp")
-	flag.IntVar(&NumChars, "n", 64, "number of characters to print")
+	flag.IntVar(&NumChars, "n", 40, "number of characters to print")
+	flag.StringVar(&Message, "m", "", "commit message")
+}
+
+type Snapshot struct {
+	ID            string
+	Message       string
+	Time          string
+	Files	      []string
+	StoredFiles	  []string
 }
 
 func main() {
-	os.Mkdir(".gover", 0777)
-
 	flag.Parse()
 
-		for i := 0; i < flag.NArg(); i++ {
-			fileName := flag.Arg(i)
-			ext := filepath.Ext(fileName)
-			hash, _ := HashFile(fileName, NumChars)
+	// Optional timestamp
+	t := time.Now()
+	ts := t.Format("2006-01-02T15-04-05")
+	snap := Snapshot{Time: ts}
+	snap.Files = []string{}
+	snap.StoredFiles = []string{}
+	snap.Message = Message
 
-			verFolder := filepath.Join(".gover", "data", hash[0:2]) 
-			os.MkdirAll(verFolder, 0777)
+	for i := 0; i < flag.NArg(); i++ {
+		fileName := flag.Arg(i)
+		ext := filepath.Ext(fileName)
+		hash, _ := HashFile(fileName, NumChars)
+		verFolder := filepath.Join(".gover", "data", hash[0:2]) 
+		verFile := filepath.Join(verFolder, hash + ext)
 
-			verFile := filepath.Join(verFolder, hash + ext)
-			fmt.Printf("%s -> %s\n", fileName, verFile)
-		}
+		snap.Files = append(snap.Files, fileName)
+		snap.StoredFiles = append(snap.StoredFiles, verFile)
+
+		os.MkdirAll(verFolder, 0777)
+		CopyFile(fileName, verFile)
+		fmt.Printf("%s -> %s\n", fileName, verFile)
+	}
+
+	snapFolder := filepath.Join(".gover", "snapshots")
+	os.MkdirAll(snapFolder, 0777)
+	snapFile := filepath.Join(snapFolder, ts + ".json")
+	snap.Write(snapFile)
 }
+
