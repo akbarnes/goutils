7a2819c90fcb4b261bb10ecd4a884ac1306af204
Art Barnes <32542598+akbarnes@users.noreply.github.com>
2021-06-04 07:24:28 -0600

    add subfolders

--- goversion.go
+++ goversion.go
@@ -4,9 +4,11 @@ import (
 	"crypto/sha256"
 	"flag"
 	"fmt"
+	"io"
 	"io/ioutil"
-	// "os"
-	"time"
+	"os"
+	// "time"
+	"path/filepath"
 )
 
 func check(e error) {
@@ -15,24 +17,22 @@ func check(e error) {
 	}
 }
 
-func HashFile(FileName string, NumChars int, TimeStampMode bool) (string, error) {
+// // Optional timestamp
+// t := time.Now()
+
+// if TimeStampMode {
+// 	fmt.Print(t.Format("2006-01-02T15-04-05-"))
+// }
+
+func HashFile(FileName string, NumChars int) (string, error) {
 	var data []byte
 	var err error
-
-	// Optional timestamp
-	t := time.Now()
-
-	if TimeStampMode {
-		fmt.Print(t.Format("2006-01-02T15-04-05-"))
-	}
-
 
 	data, err = ioutil.ReadFile(FileName)
 
 	if err != nil {
 		return "", err
 	}
-
 
 	sum := fmt.Sprintf("%x", sha256.Sum256(data))
 
@@ -43,19 +43,50 @@ func HashFile(FileName string, NumChars int, TimeStampMode bool) (string, error)
 	return sum[0:NumChars], nil
 }
 
+// Copy the source file to a destination file. Any existing file
+// will be overwritten and will not copy file attributes.
+func CopyFile(src, dst string) error {
+	in, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer in.Close()
+
+	out, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	_, err = io.Copy(out, in)
+	if err != nil {
+		return err
+	}
+	return out.Close()
+}
+
 var TimeStampMode bool
 var NumChars int
 
 func init() {
 	flag.BoolVar(&TimeStampMode, "t", false, "prefix with time stamp")
-	flag.IntVar(&NumChars, "n", 32, "number of characters to print")
+	flag.IntVar(&NumChars, "n", 64, "number of characters to print")
 }
 
 func main() {
+	os.Mkdir(".gover", 0777)
+
 	flag.Parse()
 
 		for i := 0; i < flag.NArg(); i++ {
-			hash, _ := HashFile(flag.Arg(i), NumChars, TimeStampMode)
-			fmt.Printf("%s -> %s\n", flag.Arg(i), hash)
+			fileName := flag.Arg(i)
+			ext := filepath.Ext(fileName)
+			hash, _ := HashFile(fileName, NumChars)
+
+			verFolder := filepath.Join(".gover", "data", hash[0:2]) 
+			os.MkdirAll(verFolder, 0777)
+
+			verFile := filepath.Join(verFolder, hash + ext)
+			fmt.Printf("%s -> %s\n", fileName, verFile)
 		}
 }
