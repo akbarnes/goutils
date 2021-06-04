e2a844b8064297cab339ed133033588b095e0368
Art Barnes <32542598+akbarnes@users.noreply.github.com>
2021-06-04 14:45:40 -0600

    add option to toggle file printing

--- gover.go
+++ gover.go
@@ -8,6 +8,7 @@ import (
 	"io/ioutil"
 	"os"
 	"time"
+	"math/rand"
 	"path/filepath"
 	"encoding/json"
 )
@@ -16,6 +17,27 @@ func check(e error) {
 	if e != nil {
 		panic(e)
 	}
+}
+
+var seededRand *rand.Rand = rand.New(
+	rand.NewSource(time.Now().UnixNano()))
+  
+const HexChars = "0123456789abcdef"
+
+// Return a random string of specified length with hexadecimal characters
+func RandHexString(length int) string {
+	return RandString(length, HexChars)
+}
+
+// Return a random string of specified length with an arbitrary character set
+func RandString(length int, charset string) string {
+	b := make([]byte, length)
+
+	for i := range b {
+	b[i] = charset[seededRand.Intn(len(charset))]
+	}
+
+	return string(b)
 }
 
 func HashFile(FileName string, NumChars int) (string, error) {
@@ -71,12 +93,34 @@ func (snap Snapshot) Write(snapshotPath string) {
 	f.Close()
 }
 
-var TimeStampMode bool
+// Read a snapshot given a file path
+func ReadSnapshotFile(snapshotPath string) Snapshot {
+	var mySnapshot Snapshot
+	f, err := os.Open(snapshotPath)
+
+	if err != nil {
+		// panic(fmt.Sprintf("Error: Could not read snapshot file %s", snapshotPath))
+		return Snapshot{}
+	}
+
+	myDecoder := json.NewDecoder(f)
+
+	if err := myDecoder.Decode(&mySnapshot); err != nil {
+		panic(fmt.Sprintf("Error:could not decode snapshot file %s", snapshotPath))
+	}
+
+	f.Close()
+	return mySnapshot
+}
+
+var LogCommand bool
+var LogFiles bool
 var NumChars int
 var Message string
 
 func init() {
-	flag.BoolVar(&TimeStampMode, "t", false, "prefix with time stamp")
+	flag.BoolVar(&LogCommand, "log", false, "list snapshots")
+	flag.BoolVar(&LogFiles, "f", false, "print files when listing snapshots")
 	flag.IntVar(&NumChars, "n", 40, "number of characters to print")
 	flag.StringVar(&Message, "m", "", "commit message")
 }
@@ -92,32 +136,58 @@ type Snapshot struct {
 func main() {
 	flag.Parse()
 
-	// Optional timestamp
-	t := time.Now()
-	ts := t.Format("2006-01-02T15-04-05")
-	snap := Snapshot{Time: ts}
-	snap.Files = []string{}
-	snap.StoredFiles = []string{}
-	snap.Message = Message
+	if LogCommand {
+		snapshotGlob := filepath.Join(".gover","snapshots","*.json")
+		snapshotPaths, err := filepath.Glob(snapshotGlob)
+		check(err)
 
-	for i := 0; i < flag.NArg(); i++ {
-		fileName := flag.Arg(i)
-		ext := filepath.Ext(fileName)
-		hash, _ := HashFile(fileName, NumChars)
-		verFolder := filepath.Join(".gover", "data", hash[0:2]) 
-		verFile := filepath.Join(verFolder, hash + ext)
+		for _, snapshotPath := range snapshotPaths {
+			snap := ReadSnapshotFile(snapshotPath)
 
-		snap.Files = append(snap.Files, fileName)
-		snap.StoredFiles = append(snap.StoredFiles, verFile)
+			// ID: 943e8daa (943e8daa4bc0ab899c36b5030d4a27a6b833b2ba)
+			// Time: 2021/05/08 08:57:46
+			// Message: specify workdir path explicitly
+			fmt.Printf("Time: %s\nID: %s\nMessage: %s\n", snap.Time, snap.ID, snap.Message)
 
-		os.MkdirAll(verFolder, 0777)
-		CopyFile(fileName, verFile)
-		fmt.Printf("%s -> %s\n", fileName, verFile)
+			if LogFiles {
+				fmt.Println("Files:")
+
+				for _, file := range snap.Files {
+					fmt.Printf("  %s\n",file)
+				}
+			}
+			
+			fmt.Println("")
+		}
+	} else {
+		// Optional timestamp
+		t := time.Now()
+		ts := t.Format("2006-01-02T15-04-05")
+		snap := Snapshot{Time: ts}
+		snap.Files = []string{}
+		snap.StoredFiles = []string{}
+		snap.ID = RandHexString(40)
+		snap.Message = Message
+
+		for i := 0; i < flag.NArg(); i++ {
+			fileName := flag.Arg(i)
+			ext := filepath.Ext(fileName)
+			hash, _ := HashFile(fileName, NumChars)
+			verFolder := filepath.Join(".gover", "data", hash[0:2]) 
+			verFile := filepath.Join(verFolder, hash + ext)
+
+			snap.Files = append(snap.Files, fileName)
+			snap.StoredFiles = append(snap.StoredFiles, verFile)
+
+			os.MkdirAll(verFolder, 0777)
+			CopyFile(fileName, verFile)
+			fmt.Printf("%s -> %s\n", fileName, verFile)
+		}
+
+		snapFolder := filepath.Join(".gover", "snapshots")
+		os.MkdirAll(snapFolder, 0777)
+		snapFile := filepath.Join(snapFolder, ts + ".json")
+		snap.Write(snapFile)
 	}
-
-	snapFolder := filepath.Join(".gover", "snapshots")
-	os.MkdirAll(snapFolder, 0777)
-	snapFile := filepath.Join(snapFolder, ts + ".json")
-	snap.Write(snapFile)
 }
 
